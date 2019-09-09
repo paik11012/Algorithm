1e32ee62a29f9c533e782637239bc9d088e32e33
validName <>
2019-09-09 17:54:12 +0900

    0909

--- lecture/day08_0827_stack2/02_maze_queue,BFS.py
+++ lecture/day08_0827_stack2/02_maze_queue,BFS.py
@@ -15,15 +15,14 @@ def out_idx(yy,xx):
 def bfs(y,x):
     global result
     q.append((y,x))
-    visited[y][x] = 1
-    # print(q)
     while q:  # q에 무엇이 있는 동안
         y, x = q.pop()
-        for dir in range(4):  # 네 방향을 돌면서  상 하 좌 우
-            Y = y + dy[dir]
-            X = x + dx[dir]
+        for d in range(4):  # 네 방향을 돌면서  상 하 좌 우
+            Y = y + dy[d]
+            X = x + dx[d]
             if out_idx(Y,X) == False and maze[Y][X] != 1 and visited[Y][X] == 0: # 그 노드가 밖 범위가 아니고 방문하지 않았으면
                 q.append((Y,X))  # 큐에다가 넣어라
+                print(q)
                 # print(Y, X)
                 visited[Y][X] = 1  # 방문으로 표시
                 if maze[Y][X] == 3:
@@ -43,4 +42,4 @@ dy = [1, -1, 0, 0]
 dx = [0, 0, -1, 1] # 하 상 좌 우
 bfs(starty, startx)  # 0 1
 print(result)
-# pprint(visited)
+pprint(visited)
