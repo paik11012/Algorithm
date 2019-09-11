8a019c83828d78b14e29ceb03ba7fe8f4da9ed36
paik11012 <minju11012@gmail.com>
2019-09-11 15:58:58 +0900

    0911

--- study/d3/1253_palindrome1.py
+++ study/d3/1253_palindrome1.py
@@ -2,37 +2,31 @@ import sys
 from pprint import pprint
 sys.stdin = open('1253.txt','r')
 
-
-def out_idx(i, j):
-    if i < 0 or i > 7: return True
-    if j < 0 or j > 7: return True
-    return False
-
-
 n = int(input())
 box = []
 for _ in range(8):
     box.append(list(str(input())))
 pprint(box)
 cnt = 0
-for x in range(8):
-    for y in range(8):  # 시작점 설정
-        if n % 2 == 0: # 짝수
-            flag = False
-            for z in range(n//2): # 0 1
-                if out_idx(x, y) and box[x][y] == box[x][(n-1)-y]:  # 03, 12 매칭
-                    flag = True
-                else: break
-            if not flag:  # flag가 true면
-                cnt += 1
-        else:  # 홀수
-            flag = False
-            for z in range(n//2): # 0 1
-                if out_idx(x, y) and box[x][y] == box[x][n-y]:
-                    flag = True
-                else: break
-            if not flag:
-                cnt += 1
+for x in range(8): # 0~7
+    for y in range(8-n+1):  # 시작점 설정  # 0~4까지
+        flag = False
+        for z in range(n//2): # 0 1
+            if box[x][y] == box[x][(n-1)-y]:  # 03, 12 매칭
+                flag = True
+                print(x,y,x,n-1-y)
+            else: break
+        if flag == True:  # flag가 true면
+            cnt += 1
 print(cnt)
+#         else:  # 홀수
+#             flag = False
+#             for z in range(n//2): # 0 1
+#                 if out_idx(x, y) and box[x][y] == box[x][n-y]:
+#                     flag = True
+#                 else: break
+#             if not flag:
+#                 cnt += 1
+# print(cnt)
 
 
