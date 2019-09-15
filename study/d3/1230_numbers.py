dee2502fa1cc0cf267ee71ef07e2a54470a07e5f
paik11012 <minju11012@gmail.com>
2019-09-16 08:47:40 +0900

    0915

---
+++ study/d3/1230_numbers.py
@@ -0,0 +1,13 @@
+# 1. I(삽입) x, y, s : 앞에서부터 x의 위치 바로 다음에 y개의 숫자를 삽입한다. s는 덧붙일 숫자들이다.[ ex) I 3 2 123152 487651 ]
+#
+# 2. D(삭제) x, y : 앞에서부터 x의 위치 바로 다음부터 y개의 숫자를 삭제한다.[ ex) D 4 4 ]
+#
+# 3. A(추가) y, s : 암호문의 맨 뒤에 y개의 숫자를 덧붙인다. s는 덧붙일 숫자들이다. [ ex) A 2 421257 796813 ]
+import sys
+from pprint import pprint
+sys.stdin = open('1230.txt','r')
+total_n = int(input())
+nums = list(map(int, input().split()))
+total_e = int(input())
+data = list(map(str, input().split()))
+print(data)
