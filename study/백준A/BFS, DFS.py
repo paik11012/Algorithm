1db230524b3de6d306aae3ea677119caa0c529e8
paik11012 <minju11012@gmail.com>
2019-09-25 22:53:37 +0900

    0925

--- study/백준A/BFS, DFS.py
+++ study/백준A/BFS, DFS.py
@@ -1,16 +1,30 @@
+import collections
+
 def bfs(x):
     visited = [False] * (n + 1)
-    q = []
+    q = collections.deque([])
     q.append(x)
     visited[x] = True
     while q:
-        node = q.pop(0)
+        node = q.popleft()
         print(node)
         for i in info_list[node]:
             if not visited[i]:
                 visited[i] = True
                 q.append(i)
-
+#
+# def bfs(x):
+#     visited = [False] * (n + 1)
+#     q = []
+#     q.append(x)
+#     visited[x] = True
+#     while q:
+#         node = q.pop(0)
+#         print(node)
+#         for i in info_list[node]:
+#             if not visited[i]:
+#                 visited[i] = True
+#                 q.append(i)
 
 n = 5
 info_list = [[], [2], [1, 3, 4], [2, 4, 5], [2, 3, 5], [3, 4]]
