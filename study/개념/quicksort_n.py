f521c504b71c4bf01c123d52f1b661e3c18d0187
paik11012 <minju11012@gmail.com>
2019-10-16 17:47:19 +0900

    1016

---
+++ study/개념/quicksort_n.py
@@ -0,0 +1,32 @@
+# 평균은 o(nlogn) 최악의 경우에는 o(n^2)
+def getboth(arr, pivot, myleft, myright):
+    for i in range(1, len(arr)):
+        if arr[i] <= pivot:
+            myleft.append(arr[i])
+        else:  # i가 pivot보다 더 크면 right에 더하기
+            myright.append(arr[i])
+
+
+def quicksort(arr, start, end):  # arr의 start부터 end까지 정렬하는 함수
+    if start >= end:
+        return None
+    else:
+        pivot = arr[start]
+        myleft = []
+        myright = []
+        getboth(arr, pivot, myleft, myright)  # 큰거
+
+        # 합치기
+        for k in range(len(myleft)-1):
+            arr[start+k] = myleft[k]
+        arr[len(myleft)] = pivot
+        for kk in range(len(myright)):
+            arr[start+len(myleft)+kk] = myright[kk]
+
+        quicksort(arr, start, len(myleft)-1) # 왼쪽
+        quicksort(arr, start+len(myleft)+1, end) # 오른
+
+
+n = [2, 3, 0, 7, 2, 5, 4, 3, 1, -1]
+quicksort(n, 0, 9)
+print(n)
