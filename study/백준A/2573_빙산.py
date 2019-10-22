4b37611137c5bdfcd935a1351ed4993d6946df32
paik11012 <minju11012@gmail.com>
2019-10-22 18:01:17 +0900

    1022

--- study/백준A/2573_빙산.py
+++ study/백준A/2573_빙산.py
@@ -18,40 +18,29 @@ count = [[0]*n for _ in range(m)]
 four_x = [1, 0, -1, 0]
 four_y = [0, 1, 0, -1]
 
-# c_box = copy.deepcopy(box)
-# for x in range(1, m):
-#     for y in range(1, n):
-#         if box[x][y] != 0:  # 네 방향 탐색하면서 0의 갯수 세기
-#             for i in range(4):
-#                 if box[x+four_x[i]][y+four_y[i]] == 0:
-#                     c_box[x][y] -= 1
-#                     if c_box[x][y] < 0:
-#                         c_box[x][y] = 0
-# pprint(c_box)
-# d_box = copy.deepcopy(c_box)
-# for x in range(1, m):  # 횟수 줄이기
-#     for y in range(1, n):
-#         if box[x][y] != 0: # 네 방향 탐색하면서 0의 갯수 세기
-#             for i in range(4):
-#                 if c_box[x+four_x[i]][y+four_y[i]] == 0:
-#                     d_box[x][y] -= 1
-#                     if d_box[x][y] < 0:
-#                         d_box[x][y] = 0
-# pprint(d_box)
+c_box = copy.deepcopy(box)
+for x in range(1, m):
+    for y in range(1, n):
+        if box[x][y] != 0:  # 네 방향 탐색하면서 0의 갯수 세기
+            for i in range(4):
+                if box[x+four_x[i]][y+four_y[i]] == 0:
+                    c_box[x][y] -= 1
+                    if c_box[x][y] < 0:
+                        c_box[x][y] = 0
+pprint(c_box)
 
 
-four_x = [1, 0, -1, 0]
-four_y = [0, 1, 0, -1]
 
-
-def bfs(x, y, cnt):  # cnt는 섬 몇개인지 세기
+def bfs(x, y):  # cnt는 섬 몇개인지 세기
+    global cntt
+    cntt += 1
     visited[x][y] = 1
     q.append((x, y))
     while q:
         X, Y = q.popleft()
         for i in range(4):  # 5m 7n
             # 범위를 넘지 않았고, 주변에 숫자가 있고, 방문하지 않았다면
-            if 0 <= X + four_x[i] < n and 0 <= Y + four_y[i]< m and box[X + four_x[i]][Y + four_y[i]] != 0 and visited[X + four_x[i]][Y + four_y[i]] == 0:
+            if 0 <= X + four_x[i] <= n and 0 <= Y + four_y[i]<= m and box[X + four_x[i]][Y + four_y[i]] != 0 and visited[X + four_x[i]][Y + four_y[i]] == 0:
                 visited[X + four_x[i]][Y + four_y[i]] = 1
                 q.append((X + four_x[i], Y + four_y[i]))
     # Q가 비면 cnt를 1씩 늘릴 것
@@ -59,8 +48,9 @@ def bfs(x, y, cnt):  # cnt는 섬 몇개인지 세기
 
 visited = [[0] * n for _ in range(m)]
 q = collections.deque([])
-for x in range(m):
-    for y in range(n):
-        if box[x][y] != 0:
-            bfs(x, y, 0)
-
+cntt = 0
+for i in range(m):
+    for j in range(n):
+        if box[i][j] != 0 and visited[i][j] == 0:
+            bfs(i, j)
+print(cntt)
