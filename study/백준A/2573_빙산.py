eff86f8810a5c664bedaea7c63e71d9751ef1b49
paik11012 <minju11012@gmail.com>
2019-10-28 17:56:06 +0900

    1028

--- study/백준A/2573_빙산.py
+++ study/백준A/2573_빙산.py
@@ -5,30 +5,10 @@ import collections
 sys.stdin = open('2573.txt', 'r')
 
 '''
-빙산이 물을 만나면 물을 만난 개수 만큼 빙산을 1씩 줄여나가고 0이 되면 물이 된다
+빙산이 물을 만나면 물을 만난 회수 만큼 빙산을 1씩 줄여나가고 0이 되면 물이 된다
 한 턴을 돌릴 때마다 빙산의 갯수가 몇 개 남았는지 확인해나가고 그 빙산의 개수가 2개가 되면
 몇 번 턴을 돌렸는지 세어 그 횟수를 반환한다
 '''
-
-m, n = map(int, input().split())
-box = []
-for _ in range(m):
-    box.append(list(map(int, input().split())))
-count = [[0]*n for _ in range(m)]
-four_x = [1, 0, -1, 0]
-four_y = [0, 1, 0, -1]
-
-c_box = copy.deepcopy(box)
-for x in range(1, m):
-    for y in range(1, n):
-        if box[x][y] != 0:  # 네 방향 탐색하면서 0의 갯수 세기
-            for i in range(4):
-                if box[x+four_x[i]][y+four_y[i]] == 0:
-                    c_box[x][y] -= 1
-                    if c_box[x][y] < 0:
-                        c_box[x][y] = 0
-pprint(c_box)
-
 
 
 def bfs(x, y):  # cnt는 섬 몇개인지 세기
@@ -43,8 +23,35 @@ def bfs(x, y):  # cnt는 섬 몇개인지 세기
             if 0 <= X + four_x[i] <= n and 0 <= Y + four_y[i]<= m and box[X + four_x[i]][Y + four_y[i]] != 0 and visited[X + four_x[i]][Y + four_y[i]] == 0:
                 visited[X + four_x[i]][Y + four_y[i]] = 1
                 q.append((X + four_x[i], Y + four_y[i]))
+    return cntt
     # Q가 비면 cnt를 1씩 늘릴 것
 
+
+def melt(i, j):
+    while bfs(i, j) < 2:
+        c_box = [[0]*m for _ in range(n)]
+        for ii in range(m):
+            for jj in range(n):
+                c_box[ii][jj] = box[ii][jj]
+
+        for x in range(1, m):
+            for y in range(1, n):
+                if box[x][y] != 0:  # 네 방향 탐색하면서 0의 갯수 세기
+                    for i in range(4):
+                        if box[x+four_x[i]][y+four_y[i]] == 0:
+                            c_box[x][y] -= 1
+                            if c_box[x][y] < 0:
+                                c_box[x][y] = 0
+        pprint(c_box)
+
+
+m, n = map(int, input().split())
+box = []
+for _ in range(m):
+    box.append(list(map(int, input().split())))
+count = [[0]*n for _ in range(m)]
+four_x = [1, 0, -1, 0]
+four_y = [0, 1, 0, -1]
 
 visited = [[0] * n for _ in range(m)]
 q = collections.deque([])
@@ -52,5 +59,4 @@ cntt = 0
 for i in range(m):
     for j in range(n):
         if box[i][j] != 0 and visited[i][j] == 0:
-            bfs(i, j)
-print(cntt)
+            melt(i, j)
