c5e9d7ace9ee439d37068e8606f7c5e00fac97fd
paik11012 <minju11012@gmail.com>
2019-10-30 00:15:42 +0900

    1030

--- study/개념/quicksort.py
+++ study/개념/quicksort.py
@@ -1,12 +1,13 @@
 # 평균은 o(nlogn) 최악의 경우에는 o(n^2)
-def getleft(arr, pivot, myleft):
-    for i in range(1, len(arr)):
+# 2n -> n번  (swap 교재에)
+def getleft(arr, pivot, start, end, myleft):
+    for i in range(start, end+1):  # 맨 앞이 pivot 이니까 빼고 돌려고 1부터 시작
         if arr[i] <= pivot:
             myleft.append(arr[i])
 
 
-def getright(arr, pivot, myright):
-    for i in range(1, len(arr)):
+def getright(arr, pivot, start, end, myright):
+    for i in range(start, end+1):
         if arr[i] > pivot:
             myright.append(arr[i])
 
@@ -18,17 +19,16 @@ def quicksort(arr, start, end):  # arr의 start부터 end까지 정렬하는 함
         pivot = arr[start]
         myleft = []
         myright = []
-        getleft(arr, pivot, myleft)  # 작은거
-        getright(arr, pivot, myright)  # 큰거
+        getleft(arr, pivot, start+1, end, myleft)  # 작은거
+        getright(arr, pivot, start+1, end, myright)  # 큰거
         # 합치기
-        for k in range(len(myleft)-1):
+        for k in range(len(myleft)):  # 왼쪽
             arr[start+k] = myleft[k]
-        arr[len(myleft)] = pivot
-        for kk in range(len(myright)):
-            arr[start+len(myleft)+kk] = myright[kk]
-
-        quicksort(arr, start, len(myleft)-1) # 왼쪽
-        quicksort(arr, start+len(myleft)+1, end) # 오른
+        arr[start+len(myleft)] = pivot  # 피봇, 기준점 정하기
+        for kk in range(len(myright)):  # 큰거
+            arr[start+len(myleft)+kk+1] = myright[kk]
+        quicksort(arr, start, start+len(myleft)-1) # 왼쪽
+        quicksort(arr, start+len(myleft)+1, end) # 오른쪽
 
 
 n = [2, 3, 0, 7, 2, 5, 4, 3, 1, -1]
--- study/백준A/2573_빙산.py
+++ study/백준A/2573_빙산.py
@@ -12,58 +12,63 @@ sys.stdin = open('2573.txt', 'r')
 '''
 
 
-def bfs(x, y):  # cnt는 섬 몇개인지 세기
-    global cntt
-    while cntt < 2:
-        global copied
-        pprint(copied)
-        again = copy.deepcopy(copied)
+def bfs(xii, yii):  # cnt는 섬 몇개인지 세기
+    global cntt, years  # 0으로 시작
+    # 녹인다
+    while cntt < 2:  # while한 번 돌 때마다 한 번 녹이는 작업
+        global copied  # 처음에는 박스 복사
+        years += 1  # 1년 추가
+        new_copied = [[0] * n for _ in range(m)]  # copied를 복사
+        for xxx in range(m):
+            for yyy in range(n):
+                new_copied[xxx][yyy] = copied[xxx][yyy]
+
         for x in range(m):
             for y in range(n):
-                if copied[x][y] != 0:  # 처음 만든 박스 참고
-                    for i in range(4):
-                        if copied[x+four_x[i]][y+four_y[i]] == 0:
-                            again[x][y] -= 1  # 바꾸는건 c_box
-                            if again[x][y] < 0:
-                                again[x][y] = 0
+                if copied[x][y] != 0:  # copied 참고
+                    for ii in range(4):
+                        if copied[x+four_x[ii]][y+four_y[ii]] == 0:
+                            new_copied[x][y] -= 1  # 바꾸는건 c_box
+                            if new_copied[x][y] < 0:
+                                new_copied[x][y] = 0
+        # 녹아서 바다로 된 부분을 처리한다
         for xi in range(m):
             for yi in range(n):
-                copied[xi][yi] = again[xi][yi]
-    return cntt
+                copied[xi][yi] = new_copied[xi][yi]
 
-    # cntt += 1
-    # visited[x][y] = 1
-    # q.append((x, y))
-    # while q:
-    #     X, Y = q.popleft()
-    #     for i in range(4):  # 5m 7n
-    #         # 범위를 넘지 않았고, 주변에 숫자가 있고, 방문하지 않았다면
-    #         if 0 <= X + four_x[i] <= n and 0 <= Y + four_y[i]<= m and box[X + four_x[i]][Y + four_y[i]] != 0 and visited[X + four_x[i]][Y + four_y[i]] == 0:
-    #             visited[X + four_x[i]][Y + four_y[i]] = 1
-    #             q.append((X + four_x[i], Y + four_y[i]))
-
-
+        # while을 한 번 돌릴 때마다 섬이 몇 개 남았는지 센다
+        cntt += 1  # 처음 들어갈 때 cntt늘리기
+        visited[xii][yii] = 1
+        q.append((xii, yii))
+        while q:
+            X, Y = q.popleft()
+            for i in range(4):  # 5m 7n
+                # 범위를 넘지 않았고, 주변에 숫자가 있고, 방문하지 않았다면
+                if 0 <= X + four_x[i] <= n and 0 <= Y + four_y[i]<= m and box[X + four_x[i]][Y + four_y[i]] != 0 and visited[X + four_x[i]][Y + four_y[i]] == 0:
+                    visited[X + four_x[i]][Y + four_y[i]] = 1
+                    q.append((X + four_x[i], Y + four_y[i]))
 
 
 m, n = map(int, input().split())
 box = []
 for _ in range(m):
     box.append(list(map(int, input().split())))
-copied = copy.deepcopy(box)
-count = [[0]*n for _ in range(m)]
+
+copied = [[0] * n for _ in range(m)]
+for xx in range(m):
+    for yy in range(n):
+        copied[xx][yy] = box[xx][yy]
+
 four_x = [1, 0, -1, 0]
 four_y = [0, 1, 0, -1]
 
 visited = [[0] * n for _ in range(m)]
 q = collections.deque([])
-cntt = -1
-
+cntt = 0
+years = 0
 for i in range(m):
     for j in range(n):
         if box[i][j] != 0 and visited[i][j] == 0:
+            cntt += 1
             bfs(i, j)
-print(cntt)
-
-
-
-
+print(cntt, years)
