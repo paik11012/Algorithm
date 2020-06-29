49849d3e7fc4098ab898956c23610079d41ef0d3
paik11012 <minju11012@gmail.com>
2020-06-29 13:54:09 +0900

    주식

--- study/kakao1/인형뽑기.py
+++ study/kakao1/인형뽑기.py
@@ -1,7 +1,15 @@
+'''
+이 문제의 키 포인트: 새로운 숫자를 넣을 때 이미 큐에 있는 맨 마지막 숫자랑 비교하자
+그리고 인형을 뽑은 후 그 자리를 0으로 바꿔 주어야 한다
+
+처음 생각한 방향: 재귀함수를 쓰면 어떨까?(새로운게 들어오면 없애고 다시 비교 시작)
+그러나 너무 복잡...
+'''
+
 def solution(board, moves):
     global answer
     copy = board
-    q = [0]
+    q = [0] # 시작 시 비교 대상이 있어야 하므로 넣기
     for a in range(len(moves)): # a갯수만큼 1 5 3 5 1 2 1 4
         for i in range(len(board)):
             if copy[i][moves[a]-1] != 0:
---
+++ study/kakao1/주식가격.py
@@ -0,0 +1,18 @@
+''' 가격이 떨어지지 않은 기간은 몇 초?
+숫자 각각을 맨 마지막 자리까지 비교하기
+'''
+
+def solution(prices):
+    answer = [0 ]* len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else: # 이전 것보다 숫자가 작아지면 떨어지면
+                answer[i] = 1
+                break
+    return answer
+
+prices = [1, 2, 2, 4, 3] # [6, 5, 1, 3, 1, 1, 0]
+print(solution(prices))
+# [1, 2, 3, 2, 3]	[4, 3, 1, 1, 0]
