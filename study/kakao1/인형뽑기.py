e2be231094880f1f3c1b1ef0c23c04c326132c85
paik11012 <minju11012@gmail.com>
2020-06-29 11:25:01 +0900

    인형뽑기완료

--- study/kakao1/인형뽑기.py
+++ study/kakao1/인형뽑기.py
@@ -1,20 +1,22 @@
 def solution(board, moves):
+    global answer
     copy = board
-    q = []
+    q = [0]
     for a in range(len(moves)): # a갯수만큼 1 5 3 5 1 2 1 4
-        for i in range(5):
+        for i in range(len(board)):
             if copy[i][moves[a]-1] != 0:
-                q.append(copy[i][moves[a]-1])
-                copy[i][moves[a]-1] = 0 # 바꿔주기    
+                if q[-1] == copy[i][moves[a]-1]: # 같으면
+                    q.pop()
+                    answer += 2
+                else: # 다르면
+                    q.append(copy[i][moves[a]-1])
+                copy[i][moves[a]-1] = 0 # 바꿔주기
                 break
-    answer = 0
-    # q = [4, 3, 1, 1, 3, 2, 4]
-    lenq = (len(q) * len(q-1)) // 2
-    print(lenq)
-    # for j in range(lenq): # 6 번 동안 확인
-    #     if q[j] == q[j+1]:
-    #         q.pop(j)
-    #         q.pop(j+1)
-    #         lenq -= 2
-    #         print(lenq)
-    # return answer
+    print(q)
+    return answer
+# [0, 4, 3, 1, 1, 3, 2, 4]
+answer = 0
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+print(solution(board, moves))
+
