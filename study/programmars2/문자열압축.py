a0f907bdcad2301fe30257c0eb0519b98dbf085c
paik11012 <minju11012@gmail.com>
2020-08-10 17:05:15 +0900

    문자열 압축

---
+++ study/programmars2/문자열압축.py
@@ -0,0 +1,19 @@
+'''모든 약수 확인하기'''
+
+def solution(s):
+    answer = 0
+    return answer
+
+
+print(solution('aabbaccc'))
+# print(solution('ababcdcdababcdcd'))
+# print(solution('abcabcdede'))
+# print(solution('abcabcabcabcdededededede'))
+# print(solution('xababcdcdababcdcd'))
+
+
+# "aabbaccc"	7
+# "ababcdcdababcdcd"	9
+# "abcabcdede"	8
+# "abcabcabcabcdededededede"	14
+# "xababcdcdababcdcd"	17
