f7ba9a4996bf014c31420d8a31ef47aeb42876a1
paik11012 <minju11012@gmail.com>
2020-08-16 21:58:00 +0900

    programmars

---
+++ study/programmars2/가장큰수_idea짱짱맨.py
@@ -0,0 +1,14 @@
+import itertools
+
+def solution(numbers):
+    big = 0
+    mypermuatation =  itertools.permutations(numbers)
+    for i in mypermuatation:
+        num = int("".join(map(str,list(i))))
+        if num > big:
+            big = num
+    return big
+
+
+# print(solution([6, 10, 2])) # 6210
+print(solution([3, 30, 34, 5, 9]))	#9534330
